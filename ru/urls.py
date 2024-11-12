289d6123cfc05e8cde3a8041b19bd28583de1a1d
PakhomCh <pakhomch@gmail.com>
2024-11-13 00:01:33 +0300

    Added grid layout for pages, page redirection, basepage for ascention game

--- ru/urls.py
+++ ru/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from django.shortcuts import redirect
 
 from . import views
 
-app_name = "polls"
+app_name = 'ru'
 urlpatterns = [
-    path("", views.home, name="home"),
+    path('', views.home, name='home'),
+    path('<str:any>/home', lambda reqest, any: redirect('/')),
+    path('<str:any>/lore', lambda reqest, any: redirect('/lore')),
+    path('<str:any>/news', lambda reqest, any: redirect('/news')),
+    path('<str:any>/login', lambda reqest, any: redirect('/login')),
+    path('<str:any>/legal', lambda reqest, any: redirect('/legal')),
+    path('dynastika/', views.dynastika, name='dynastika'),
+    path('ascention/', views.ascention, name='ascention'),
 ]
--- ru/views.py
+++ ru/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'ru/home.html', {})
+
+def ascention(request):
+    return render(request, 'ru/ascention.html', {})
+
+def dynastika(request):
+    return render(request, 'ru/dynastika.html', {})
